7351d1a3ad9af4e29811906006aab3d683fbbe68
Khattab0SY <132163778+Khattab0SY@users.noreply.github.com>
2023-05-01 20:10:10 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let div1 = document.getElementById("d1");
+let div2 = document.getElementById("d2");
+let div3 = document.getElementById("d3");
+let div4 = document.getElementById("d4");
+
+function todiv1() {
+    div1.scrollIntoView();
+}
+function todiv2() {
+    div2.scrollIntoView();
+}
+function todiv3() {
+    div3.scrollIntoView();
+}
+function todiv4() {
+    div4.scrollIntoView();
+}
